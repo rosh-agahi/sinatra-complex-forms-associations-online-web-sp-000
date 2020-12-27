3244b3a252f29c747089af8513c8dad352fc7d5c
Roshanak Agahi <rosh.agahi@gmail.com>
2020-12-27 20:40:07 +0000

    Automatically backed up by Learn

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -39,11 +39,9 @@ class OwnersController < ApplicationController
   end
   
   patch '/owners/:id' do
-    ####### bug fix
     if !params[:owner].keys.include?("pet_ids")
     params[:owner]["pet_ids"] = []
     end
-    #######
  
     @owner = Owner.find(params[:id])
     @owner.update(params["owner"])
